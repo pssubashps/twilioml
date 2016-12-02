754c12616ac588a9af3525b337b075589d7b0149
Subash <subash@subash>
2016-12-02 11:47:31 +0530

    ml changes

--- makecall.php
+++ makecall.php
@@ -3,14 +3,14 @@ include "vendor/autoload.php";
 
 
 use Twilio\Twiml;
-
+$phone = $_POST['PhoneNumber'];
 
         $response = new Twiml();
         $callerIdNumber = '919496842554';
 
         $dial = $response->dial(['callerId' => $callerIdNumber]);
 
-        $phoneNumberToDial = '+919901057647';
+        $phoneNumberToDial = $phone;
 
          $dial->number($phoneNumberToDial);
 
